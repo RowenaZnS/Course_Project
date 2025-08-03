4fd7a653ced92614d81dfd1a1d18fd2277934598
UFO\Rowena <22097739d@connect.polyu.hk>
2025-08-03 14:43:20 -0400

    Squashed 'TIF360ProjectI-Deep-Puzzle-Solver/' content from commit b5615c4
    git-subtree-dir: TIF360ProjectI-Deep-Puzzle-Solver
    git-subtree-split: b5615c40458e9afd788d04049a490d1c5692ef6f

---
+++ colorization/colorizers/resnet_v1.py
@@ -0,0 +1,123 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from IPython import embed
+
+from .base_color import *
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.downsample = downsample
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class ResNetGenerator(BaseColor):
+    def __init__(self, norm_layer=nn.BatchNorm2d):
+        super(ResNetGenerator, self).__init__()
+
+
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = norm_layer(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+
+        self.layer1 = self._make_layer(64, 64, 2)
+        self.layer2 = self._make_layer(64, 128, 2, stride=2)
+        self.layer3 = self._make_layer(128, 256, 2, stride=2)
+        self.layer4 = self._make_layer(256, 512, 2, stride=1, dilation=2)
+
+
+        self.layer5 = self._make_dilation_layer(512, 512, 3, dilation=2)
+
+
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1, bias=True),
+            nn.ReLU(True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.ReLU(True),
+            nn.Conv2d(256, 313, kernel_size=1, stride=1, padding=0, bias=True)
+        )
+
+        self.softmax = nn.Softmax(dim=1)
+        self.model_out = nn.Conv2d(313, 2, kernel_size=1, padding=0, dilation=1, stride=1, bias=False)
+        self.upsample4 = nn.Upsample(scale_factor=4, mode='bilinear')
+
+    def _make_layer(self, in_channels, out_channels, blocks, stride=1, dilation=1):
+        downsample = None
+        if stride != 1 or in_channels != out_channels:
+            downsample = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+
+        layers = []
+        layers.append(ResidualBlock(in_channels, out_channels, stride, downsample))
+
+        for _ in range(1, blocks):
+            layers.append(ResidualBlock(out_channels, out_channels))
+
+        return nn.Sequential(*layers)
+
+    def _make_dilation_layer(self, in_channels, out_channels, blocks, dilation=2):
+        layers = []
+        for _ in range(blocks):
+            layers.append(nn.Conv2d(in_channels, out_channels, kernel_size=3,
+                                    dilation=dilation, stride=1, padding=dilation, bias=False))
+            layers.append(nn.BatchNorm2d(out_channels))
+            layers.append(nn.ReLU(inplace=True))
+            in_channels = out_channels
+
+        return nn.Sequential(*layers)
+
+    def forward(self, input_l):
+        x = self.normalize_l(input_l)
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.layer5(x)
+
+
+        x = self.decoder(x)
+        out_reg = self.model_out(self.softmax(x))
+
+        return self.unnormalize_ab(self.upsample4(out_reg))
+
+
+def resnet_colorizer(pretrained=False):
+    model = ResNetGenerator()
+    if pretrained:
+
+        pass
+    return model
